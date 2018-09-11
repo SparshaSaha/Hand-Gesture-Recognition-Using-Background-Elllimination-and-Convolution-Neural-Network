b408e0c953c86b6ab0563491e66110042b33131a
sparsha <saha.sparsha@gmail.com>
2018-09-11 23:43:47 +0530

    Model trained and progressed

---
+++ ResizeImages.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+def resizeImage(imageName):
+    basewidth = 100
+    img = Image.open(imageName)
+    wpercent = (basewidth/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+    img.save(imageName)
+
+for i in range(0, 11):
+    resizeImage("SwingTest/swing_" + str(i) + '.png')
