de0cd943e2e9406f72f911bb23e0a66bf215bec6
mohammed-Emad <mohammed.imad38@yahoo.com>
2019-06-04 03:02:40 +0200

    skip  'NoneType' object has no attribute 'shape'
    Create a condition to warn that there is no entry and override the mysterious message
     [AttributeError: 'NoneType' object has no attribute 'shape' ]

--- PalmTracker.py
+++ PalmTracker.py
@@ -60,69 +60,73 @@ def main():
     while(True):
         # get the current frame
         (grabbed, frame) = camera.read()
+        if (grabbed == True):
 
-        # resize the frame
-        frame = imutils.resize(frame, width=700)
+            # resize the frame
+            frame = imutils.resize(frame, width=700)
 
-        # flip the frame so that it is not the mirror view
-        frame = cv2.flip(frame, 1)
+            # flip the frame so that it is not the mirror view
+            frame = cv2.flip(frame, 1)
 
-        # clone the frame
-        clone = frame.copy()
+            # clone the frame
+            clone = frame.copy()
 
-        # get the height and width of the frame
-        (height, width) = frame.shape[:2]
+            # get the height and width of the frame
+            (height, width) = frame.shape[:2]
 
-        # get the ROI
-        roi = frame[top:bottom, right:left]
+            # get the ROI
+            roi = frame[top:bottom, right:left]
 
-        # convert the roi to grayscale and blur it
-        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+            # convert the roi to grayscale and blur it
+            gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
-        # to get the background, keep looking till a threshold is reached
-        # so that our running average model gets calibrated
-        if num_frames < 30:
-            run_avg(gray, aWeight)
-            print(num_frames)
-        else:
-            # segment the hand region
-            hand = segment(gray)
+            # to get the background, keep looking till a threshold is reached
+            # so that our running average model gets calibrated
+            if num_frames < 30:
+                run_avg(gray, aWeight)
+                print(num_frames)
+            else:
+                # segment the hand region
+                hand = segment(gray)
 
-            # check whether hand region is segmented
-            if hand is not None:
-                # if yes, unpack the thresholded image and
-                # segmented region
-                (thresholded, segmented) = hand
+                # check whether hand region is segmented
+                if hand is not None:
+                    # if yes, unpack the thresholded image and
+                    # segmented region
+                    (thresholded, segmented) = hand
 
-                # draw the segmented region and display the frame
-                cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
-                if start_recording:
+                    # draw the segmented region and display the frame
+                    cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
+                    if start_recording:
 
-                    # Mention the directory in which you wanna store the images followed by the image name
-                    cv2.imwrite("Dataset/FistTest/fist_" + str(image_num) + '.png', thresholded)
-                    image_num += 1
-                cv2.imshow("Thesholded", thresholded)
+                        # Mention the directory in which you wanna store the images followed by the image name
+                        cv2.imwrite("Dataset/FistTest/fist_" + str(image_num) + '.png', thresholded)
+                        image_num += 1
+                    cv2.imshow("Thesholded", thresholded)
 
-        # draw the segmented hand
-        cv2.rectangle(clone, (left, top), (right, bottom), (0,255,0), 2)
+            # draw the segmented hand
+            cv2.rectangle(clone, (left, top), (right, bottom), (0,255,0), 2)
 
-        # increment the number of frames
-        num_frames += 1
+            # increment the number of frames
+            num_frames += 1
 
-        # display the frame with segmented hand
-        cv2.imshow("Video Feed", clone)
+            # display the frame with segmented hand
+            cv2.imshow("Video Feed", clone)
 
-        # observe the keypress by the user
-        keypress = cv2.waitKey(1) & 0xFF
+            # observe the keypress by the user
+            keypress = cv2.waitKey(1) & 0xFF
 
-        # if the user pressed "q", then stop looping
-        if keypress == ord("q") or image_num > 100:
-            break
+            # if the user pressed "q", then stop looping
+            if keypress == ord("q") or image_num > 100:
+                break
         
-        if keypress == ord("s"):
-            start_recording = True
+            if keypress == ord("s"):
+                start_recording = True
 
+        else:
+            print("[Warning!] Error input, Please check your(camra Or video)")
+            break
 main()
 
 # free up memory
